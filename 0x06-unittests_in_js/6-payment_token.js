e4c6de3460c81384d7f0ed8fda384ec03dbc0f53
KingKidd4110 <ayiendabrian@gmail.com>
2023-08-25 15:26:49 +0300

    capalot

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+function getPaymentTokenFromAPI(success) {
+    if (succcess) {
+        return new promise((resove) => {
+            resolve({data: 'Successful response from API'});
+        });
+    }
+}
+
+module.exports = getPaymentTokenFromAPI;
